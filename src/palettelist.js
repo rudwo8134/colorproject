885d4a0eb69751befb43dccd0d8b5fb239252e2e
rudwo8134 <rudwo8134@gmail.com>
2021-05-11 21:57:51 -0400

    add style.css

--- src/palettelist.js
+++ src/palettelist.js
@@ -1,11 +1,13 @@
 import React,{Component} from "react"
 import {Link} from 'react-router-dom'
+import MiniPalette from './minipalette'
 
 class palettelist extends Component{
   render(){
       const {palette} = this.props
       return(
           <div>
+              <MiniPalette/>
               <h1>Eric's Colors</h1>
               {palette.map(pa=>(
                   <p>
