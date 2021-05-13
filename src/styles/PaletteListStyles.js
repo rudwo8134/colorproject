aa70d12b25a388ec6044f4bb8f2af90826fbb898
rudwo8134 <rudwo8134@gmail.com>
2021-05-13 17:42:08 -0400

    add the background

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,21 @@
 import sizes from './sizes'
+import bg from './bg.svg'
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+     /* background by SVGBackgrounds.com */
+    backgroundColor: "#000000",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll",
+     "& h1":{
+       fontSize: "2rem",
+       [sizes.down("sm")]:{
+         fontSize: "1.5rem"
+       }
+     }
   },
   container: {
     width: "50%",
@@ -41,7 +51,7 @@ export default {
     },
     [sizes.down("sm")]:{
       gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1rem"
+      gridGap: "1.4rem"
     },
   }
 };
